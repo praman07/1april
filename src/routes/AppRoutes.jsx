80a1ee34a5ac391d60b3fd17f542ce67ecfd7d43
Praman Bhogal <your-email@gmail.com>
2026-04-02 00:39:23 +0530

    fix: correct case sensitive imports

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import MainLayout from "../layouts/mainlayout";
+import MainLayout from "../layouts/Mainlayout";
 import Dashboard from "../pages/Dashboard";
 import Crypto from "../pages/Crypto";
 
